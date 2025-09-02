8eb76e66df1eb4f7c77b2c10d1bab8f4c64973cf
DinuChandran <dinuc.plr@gmail.com>
2025-09-02 17:58:05 +0530

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <QApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreationFailed,
+        &app,
+        []() { QCoreApplication::exit(-1); },
+        Qt::QueuedConnection);
+    engine.loadFromModule("CaseStudy1", "Main");
+
+    return app.exec();
+}
